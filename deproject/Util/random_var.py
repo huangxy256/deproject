2bc8778807fa7616e230453bc864451152c60193
huangxy256 <xhuang@gruyere.astro.sunysb.edu>
2024-04-26 15:03:25 -0700

    sample ramdom variable, small debug

--- deproject/Util/random_var.py
+++ deproject/Util/random_var.py
@@ -2,7 +2,7 @@ import numpy as np
 from scipy.interpolate import interp1d
 import matplotlib.pyplot as plt
 
-def draw_from_pdf(x_pdf, y_pdf, num, plot_cdf=0, plot_hist=0, bins_hist=10):
+def Draw_from_pdf(x_pdf, y_pdf, num, plot_cdf=0, plot_hist=0, bins_hist=10):
     """draw random variable from a probability density function
 
     Args:
